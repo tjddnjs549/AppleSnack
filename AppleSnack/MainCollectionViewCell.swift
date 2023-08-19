73b7268b9e628e69000997fc90171186fa8ca129
Umm <thdwjddma@naver.com>
2023-08-19 18:16:59 +0900

    CollectionView Cell Label 크기에 맞게 설정되도록 함

--- AppleSnack/MainCollectionViewCell.swift
+++ AppleSnack/MainCollectionViewCell.swift
@@ -7,6 +7,25 @@
 
 import UIKit
 
-class MainCollectionViewCell: UICollectionViewCell {
-    @IBOutlet var mainCollectionViewCell: UILabel!
+class Cell: UICollectionViewCell {
+    @IBOutlet var newCell: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    let label: UILabel = {
+            let label = UILabel()
+            label.textAlignment = .center
+        label.numberOfLines = 0 // Allow multiple lines
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+    
+    override var isSelected: Bool {
+        didSet {
+            contentView.backgroundColor = isSelected ? UIColor.lightGray : UIColor.white
+        }
+    }
+    func configure(text: String) {
+        label.text = text
+    }
+    
 }
--- AppleSnack/ViewController.swift
+++ AppleSnack/ViewController.swift
@@ -7,15 +7,19 @@
 
 import UIKit // Foundation 프레임워크를 내부적으로 import하고 있음
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     
-    fileprivate var systemImageNameArray = ["클래스", "구조체", "테스트", "일요일"]
+    var data: [String] = ["클래스", "구조체"]
+    
     
     @IBOutlet weak var myCollectionView: UICollectionView!
+    @IBOutlet weak var newButton: UIButton!
     @IBOutlet weak var floatingStackView: UIStackView!
     @IBOutlet weak var floatingButton: UIButton!
     @IBOutlet weak var fixButton: UIButton!
     @IBOutlet weak var deletButton: UIButton!
+    
+    
     
     lazy var floatingDimView: UIView = {
         let view = UIView(frame: self.view.frame)
@@ -31,33 +35,57 @@ class ViewController: UIViewController {
     
     var isShowFloating: Bool = false
     
-    lazy var buttons: [UIButton] = [self.fixButton, self.deletButton]
-   
+    lazy var buttons: [UIButton] = [self.fixButton, self.deletButton, self.newButton]
+    
+    
+    
+    // MARK: - viewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // MARK: - Lifecycles
         
         // Layout 간격 설정
-        let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        flowLayout.itemSize = CGSize(width: 100, height: 50)
-//        myCollectionView.collectionViewLayout = flowLayout // 기본 레이아웃으로 설정?!
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        
+        
+        if let flowLayout = myCollectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
+        
+        // flowLayout = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
+        flowLayout.register(Cell.self, forDecorationViewOfKind: "Cell")
+        flowLayout.minimumLineSpacing = 0.5 // 셀의 세로 간격
+        flowLayout.minimumInteritemSpacing = 0.5 // 셀의 가로 간격
+        
+        
+        // flowLayout.itemSize = CGSize(width: 100, height: 50)
+        // myCollectionView.collectionViewLayout = flowLayout // 기본 레이아웃으로 설정?!
         
         
         // 콜렉션 뷰에 대한 설정
         myCollectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         myCollectionView.dataSource = self
         myCollectionView.delegate = self
-
         
+        
+        //        let viewModel = CommentViewModel(comment: comments[IndexPath.row])
+        //        let height = viewModel.size(forWidth: view.frame.width).height
+        //        return CGSize(width: view.frame.width, height: height)
     }
+    
+    
+    
+    // MARK: - FloationgButton
     
     @IBAction func floatingButtonAction(_ sender: UIButton) {
         
         if isShowFloating {
             buttons.reversed().forEach { button in
                 UIView.animate(withDuration: 0.3){
-                    button.isHidden = true
+                    button.isHidden = true // "편집" 버튼을 눌렀을 때 다시 접히게 해줌
                     self.view.layoutIfNeeded()
                     
                 }
@@ -85,7 +113,6 @@ class ViewController: UIViewController {
                     
                 }
             }
-            
         }
         
         isShowFloating = !isShowFloating
@@ -98,60 +125,75 @@ class ViewController: UIViewController {
             sender.transform = roatation
         }
     }
-}
+    
+    // MARK: - UICollectionViewDataSource
     
     
-extension ViewController: UICollectionViewDataSource {
     // 지정된 섹션에 표시할 셀의 개수를 묻는 메서드
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.systemImageNameArray.count // 내가 표시할 컬렉션뷰의 개수
-        
+        return data.count // 내가 표시할 컬렉션뷰의 개수
     }
     
-    // 각 컬렉션뷰 셀에 대한 설정 or 컬렉션 뷰의 지정된 위치에 표시할 셀울 요청하는 메서드
+    // 각 컬렉션뷰 셀에 대한 설정 or 컬렉션 뷰의 지정된 위치에 표시할 셀을 요청하는 메서드
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        // 셀의 인스턴스
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainCollectionViewCell", for: indexPath) as! MainCollectionViewCell
+        // 다운케스팅 - 셀의 인스턴스
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
+        
+        // 데이터에 따른 UI 변경
+        // 라벨 설정
+        // cell.Cell.text = self.data[indexPath.item]
+        // cell.label.addTarget(self, action: #selector(addCellButtonTapped(_ :)), for: .touchUpInside)
         
         // 배경화면
         cell.contentView.backgroundColor = UIColor.systemBlue
         cell.contentView.layer.cornerRadius = 25
         cell.contentView.layer.borderWidth = 1
+        cell.newCell.text = data[indexPath.row] // cell에 입력한 label이 나오게 해줌
         
-        // 데이터에 따른 UI 변경
-        // 라벨 설정
-        cell.mainCollectionViewCell.text = self.systemImageNameArray[indexPath.item]
+        //        cell.configure(text: data[indexPath.item])
+        cell.deleteButton.tag = indexPath.item
+        cell.deleteButton.addTarget(self, action: #selector(deletButton(_ :)), for: .touchUpInside)
         
         
         
         return cell
     }
     
-    //        return UICollectionViewCell() // 내가 표시하고자하는 셀
-}
-
-
-extension ViewController : UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if let cell = collectionView.cellForItem(at: indexPath) {
+    @objc func addCellButtonTapped(_ sender: UIButton) {
+        let indexPath = IndexPath(item: data.count, section: 0)
+        data.append("Cell \(data.count + 1)")
+        myCollectionView.insertItems(at: [indexPath])
+    }
+    // return UICollectionViewCell() // 내가 표시하고자하는 셀
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    
+    private func myCollectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width: CGFloat = (collectionView.frame.width / 3) - 0.5
+        
+        return CGSize(width: width, height: width)
+        
+        if let cell = myCollectionView.cellForItem(at: indexPath) {
             cell.contentView.backgroundColor = .green
+            print("작동중")
             
             // 셀을 선택했을 때 호출해서 페이지 전환
-//            switch indexPath.item {
-//            // 첫 번째 셀 선택 시, 다른 페이지로 이동하는 동작
-//
-//            case 0:
-//                let nextpageController = NextPageViewController() // 이동할 뷰 컨트롤러 인스턴스 생성
-//                navigationController?.pushViewController(nextpageController, animated: true)
-//
-//            case 1:
-//                // 두 번째 셀 선택 시, 다른 페이지로 이동하는 동작을 구현
-//
-//            default:
+            //            switch indexPath.item {
+            //            // 첫 번째 셀 선택 시, 다른 페이지로 이동하는 동작
+            //
+            //            case 0:
+            //                let nextpageController = NextPageViewController() // 이동할 뷰 컨트롤러 인스턴스 생성
+            //                navigationController?.pushViewController(nextpageController, animated: true)
+            //
+            //            case 1:
+            //                // 두 번째 셀 선택 시, 다른 페이지로 이동하는 동작을 구현
+            //
+            //            default:
             
-//                break
-//            }
+            //                break
+            //            }
         }
     }
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
@@ -160,7 +202,43 @@ extension ViewController : UICollectionViewDelegate {
             cell.contentView.backgroundColor = .systemBlue
         }
     }
-
+    
+    // cell 크기 조정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+// let text = data[indexPath.item]
+// let width = collectionView.bounds.width - 20
+        // let height = text.height(withConstrainedWidth: width, font: UIFont.systemFont(ofSize: 10))
+        
+        return CGSize(width: 100, height: 50)
+    }
+    // MARK: - alert Button Action
+    
+    @IBAction func newButton(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "카테고리를 생성합니다.", message: "", preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = "입력하세요"
+        }
+        
+        let addAction = UIAlertAction(title: "생성", style: .default) { [weak self, weak alertController] _ in
+            if let textField = alertController?.textFields?.first, let text = textField.text, !text.isEmpty {
+                self?.data.append(text)
+                self?.myCollectionView.reloadData()
+            }
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+        
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    // MARK: - deletButton
+    
+    @IBAction func deletButton(_ sender: UIButton) {
+        
+    }
 }
-
-//
