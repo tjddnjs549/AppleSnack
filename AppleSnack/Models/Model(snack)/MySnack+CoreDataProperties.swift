add8d0b7b2dab34f7777a591210e9ee851e0b9a5
dongju <131073398+Dongjuice@users.noreply.github.com>
2023-08-21 11:45:41 +0900

    테이블 뷰 수정

--- AppleSnack/Models/Model(snack)/MySnack+CoreDataProperties.swift
+++ AppleSnack/Models/Model(snack)/MySnack+CoreDataProperties.swift
@@ -21,6 +21,14 @@ extension MySnack {
     @NSManaged public var date: Date?
     @NSManaged public var text: String?
     @NSManaged public var title: String?
+    
+    var dateString: String? {
+            let myFormatter = DateFormatter()
+            myFormatter.dateFormat = "M월 dd일 EE요일"
+            guard let date = self.date else { return "" }
+            let savedDateString = myFormatter.string(from: date)
+            return savedDateString
+        }
 
 }
 
